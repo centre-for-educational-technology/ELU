f376b73b4de94716d14c7671025e0ed484918c10
GlebRed <glebredko@gmail.com>
2017-01-26 19:38:11 +0200

    References #30

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -175,5 +175,18 @@
             +372 6409236 / <a href="mailto:elu@tlu.ee">elu@tlu.ee</a></p>
     </footer>
 </div>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5887a6e47892882db719ca58/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+  })();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 </html>

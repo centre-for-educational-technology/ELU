c340d3e85afe36268bf123ef5fe7e4c5552ca7a2
GlebRed <glebredko@gmail.com>
2017-03-06 21:14:57 +0200

    Seeder to make storage/projects_featured_images directories

---
+++ database/seeds/MakeFeaturedImagesFolderSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+
+class MakeFeaturedImagesFolderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      File::makeDirectory(public_path('storage/projects_featured_images'), 0755, true);
+    }
+}

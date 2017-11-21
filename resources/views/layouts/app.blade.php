e05bfd0c90c00fa0399cf1f935f371d2ae463f91
randotm <randotm@users.noreply.github.com>
2017-11-21 15:11:10 +0000

    EU logo moved to footer, closes #129.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -140,10 +140,6 @@
 
                 <a class="navbar-brand" href="{{url('/')}}"><img src="{{ url(asset('/css/logo.svg')) }}" alt="Tallinna Ülikool"></a>
 
-                <div class="col-sm-2" id="eu_fund_logo_container">
-                    <img src="{{ url(asset('/css/eu_fund_logo.jpg')) }}" alt="EU Fund" class="img-responsive" id="eu_fund_logo">
-                </div>
-
             </div>
             <div id="navbar" class="navbar-collapse collapse pull-right">
 

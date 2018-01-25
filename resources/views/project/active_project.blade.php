c8ef59e6fe496d9637184b3aa29b72c9c061616e
randotm <randotm@users.noreply.github.com>
2018-01-25 11:50:26 +0200

    Hardcoded end date for members leaving a project (ref #152) (#154)
    * Re-enabled leaving from the project, gooing to have to manually disable after the end of signing up period.
    
    * Changed the point until which members can remove themselves from the project

--- resources/views/project/active_project.blade.php
+++ resources/views/project/active_project.blade.php
@@ -320,7 +320,8 @@
         <h3><span class="glyphicon ico-inspire"></span>{{trans('search.join')}}</h3>
 
         {{--Check for join deadline--}}
-        @if (Carbon\Carbon::today()->format('Y-m-d') > Str::limit($project->join_deadline, 10, ''))
+        {{--@if (Carbon\Carbon::today()->format('Y-m-d') > Str::limit($project->join_deadline, 10, ''))--}}
+        @if (Carbon\Carbon::today()->format('Y-m-d') > '2018-02-05')
             <p class="red"><i class="fa fa-btn fa-frown-o"></i>{{trans('project.deadline_over')}}</p>
         @else
             @if(Auth::check())

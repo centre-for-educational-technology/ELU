51a1a533238065b89c1ce555cbf882ba59cca64d
randotm <randotm@users.noreply.github.com>
2018-05-09 11:09:58 +0300

    Viewing projects for guests should be a possibility again.

--- resources/views/project/all.blade.php
+++ resources/views/project/all.blade.php
@@ -216,7 +216,7 @@
                         @if (Carbon\Carbon::today()->format('Y-m-d') > Str::limit($project->join_deadline, 10, ''))
                         {{--@if (Carbon\Carbon::today()->format('Y-m-d') > '2018-02-05')--}}
                             <p class="red"><i class="fa fa-btn fa-frown-o"></i>{{trans('project.deadline_over')}}</p>
-                            @if ($project->currentUserIs('member'))
+                            @if (!Auth::guest() && $project->currentUserIs('member'))
                             <form action="{{ url('finish/'.$project->id) }}">
                                 <button type="submit" class="btn btn-danger btn-lg">{{trans('project.add_materials')}}</button>
                             </form>

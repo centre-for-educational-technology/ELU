baaea128627b4f9f70a925eec82224815e9735a8
randotm <randotm@users.noreply.github.com>
2018-08-06 14:49:49 +0300

    datepicker...

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -338,7 +338,7 @@ jQuery(document).ready(function($) {
     outerDiv.append(textareaDiv);
     $(iconSubCalendarInput).pickadate();
     $(iconCalendar).on('click', function () {
-      ($(this).parent().siblings().children('input')[0]).pickadate('open');
+      $($(this).parent().siblings().children('input')[0]).pickadate('open');
     })
     return outerDiv;
   }

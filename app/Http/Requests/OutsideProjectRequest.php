f3c43d00c6013cbc47734a8d2f5949006bf73839
randotm <randotm@users.noreply.github.com>
2018-08-08 01:07:38 +0300

    Trying to see that correct project

--- app/Http/Requests/OutsideProjectRequest.php
+++ app/Http/Requests/OutsideProjectRequest.php
@@ -24,7 +24,7 @@ class OutsideProjectRequest extends Request
     public function rules()
     {
         $rules = [
-            'name_et' => 'required|max:255',
+            'name_et' => 'required|uniqe|max:255',
             'description_et' => 'required|max:9000',
             'project_outcomes_et' => 'required|max:9000',
             'keywords_et' => 'required|max:9000',

4a62da678073a681ffb980a4e16819cc5608170a
randotm <randotm@users.noreply.github.com>
2018-08-08 00:37:54 +0300

    outsideproject blongs to users.

--- app/OutsideProject.php
+++ app/OutsideProject.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class OutsideProject extends Model
 {
-    //
+    public function users()
+  {
+    return $this->belongsToMany('App\User')->withPivot('participation_role');
+  }
 }

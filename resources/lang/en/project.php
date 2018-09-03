379d5c2628131a5c2e5fbbd38dfdba1af0326faf
randotm <randotm@users.noreply.github.com>
2018-09-03 19:22:48 +0300

    Deadline_over translation changed back.

--- resources/lang/en/project.php
+++ resources/lang/en/project.php
@@ -43,7 +43,7 @@ return [
     'no_project_found_desc_logged' => 'Add a project or project idea',
 		'no_project_found_desc_not_logged' => 'Log in and add a project or project idea',
     'language' => 'Language',
-    'deadline_over' => 'Registration to this project opens on 3rd of September!',
+    'deadline_over' => 'Registration period is over',
     'share_fb' => 'Share on Facebook',
     'share_twitter' => 'Share on Twitter',
     'joined_project_notification' => 'You have joined the project (No need to join on ÕIS)',

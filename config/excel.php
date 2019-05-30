a15bcce3f42644815596cd18b2a62a4f20f03b2f
NeoHack0101 <31630039+NeoHack0101@users.noreply.github.com>
2019-05-30 15:21:20 +0300

    Change Excel encoding to read est chars

--- config/excel.php
+++ config/excel.php
@@ -438,8 +438,8 @@ return array(
 
         'encoding'                => [
 
-            'input'  => 'UTF-8',
-            'output' => 'UTF-8'
+            'input'  => 'windows-1250',
+            'output' => 'windows-1250'
 
         ],
 
